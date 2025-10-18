dc83463572f23ef87babc4e10858ab45e7b08ec4
devolos <utk27032005@gmail.com>
2025-10-18 09:04:13 +0530

    feat: update TemplateSelector component; refactor state management and enhance image styling

--- src/components/TemplateSelector.tsx
+++ src/components/TemplateSelector.tsx
@@ -2,7 +2,7 @@
 import { resumeAtom, resumeShowCaseIdxAtom } from "@/app/store";
 import { mockBerlinData, sampleData } from "@/lib/constants";
 import { ResumeData } from "@/lib/resume-types";
-import { useSetAtom } from "jotai";
+import { useAtom, useSetAtom } from "jotai";
 import Image from "next/image";
 import { MouseEventHandler } from "react";
 
@@ -19,7 +19,7 @@ function ResumeImage({
     <Image
       src={src}
       alt={alt}
-      className="h-52 rounded-sm"
+      className="h-52 rounded-sm cursor-pointer active:scale-95 transition-all"
       height={400}
       width={200}
       onClick={onClick}
@@ -28,7 +28,7 @@ function ResumeImage({
 }
 
 export default function TemplateSelector() {
-  const setIdx = useSetAtom(resumeShowCaseIdxAtom);
+  const [index, setIdx] = useAtom(resumeShowCaseIdxAtom);
   const setResume = useSetAtom(resumeAtom);
   const images = [
     { imagePath: "/resume-simple.jpeg", defaultResume: sampleData },
@@ -43,12 +43,16 @@ export default function TemplateSelector() {
   return (
     <div className="grid grid-cols-2 gap-3">
       {images.map((data, key) => (
-        <ResumeImage
-          key={key}
-          src="/resume-simple.jpeg"
-          alt={data.imagePath.slice(1)}
-          onClick={() => handleClick(key, data.defaultResume)}
-        />
+        <div
+          className={index == key ? "border p-0.5 border-white rounded-lg" : ""}
+        >
+          <ResumeImage
+            key={key}
+            src="/resume-simple.jpeg"
+            alt={data.imagePath.slice(1)}
+            onClick={() => handleClick(key, data.defaultResume)}
+          />
+        </div>
       ))}
     </div>
   );
